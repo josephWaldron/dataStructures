9a74370d9f6a72285a5f7519e5cdcce7534a6654
josephwaldron <waldrojo@kean.edu>
2022-01-23 15:40:26 -0500

    test on laptop

--- test1.java
+++ test1.java
@@ -1,6 +1,8 @@
 public class test1 {
     public static void main(String[] args) {
         System.out.println("my name is Joseph");
-        System.out.println("test number 2");
+        System.out.println("test number 1");
+        int x = 12;
+        System.out.println(x);
     }
 }
