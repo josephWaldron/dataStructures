9346060681959fd9d254d89011cef9df6ebe3f68
JosephWaldron <waldrojo@kean.edu>
2022-01-24 12:40:15 -0500

    added template for submission

--- test1.java
+++ test1.java
@@ -1,3 +1,12 @@
+/******************************************************************************* *
+* Kean University
+* Spring 2022
+* Course: CPS*2232 – Data Structures
+* Author: Your Full Name, course, section
+* Homework (or Lab): 1, task 1, 2, 3, ….
+* Program Description:
+*************************************************************************/
+
 public class test1 {
     public static void main(String[] args) {
         System.out.println("my name is Joseph");
